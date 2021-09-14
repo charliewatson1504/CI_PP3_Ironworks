4f981c8d86147cad6c5bf73e4e7d16acc790ef2f
Charlie Watson <charliewatson87@gmail.com>
2021-09-14 19:45:35 +0000

    feat: create login function for users and provide feedback if invalid data is entered

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ which sessions they have booked and with who.
 All data required to be stored is done so on a google spreadsheet.
 """
 
-import google_sheet
+import google_sheet as gs
 
 
 def welcome_screen():
@@ -14,10 +14,12 @@ def welcome_screen():
     account
     """
     while True:
+        print('Welcome to the Ironworks Personal Training Booking System\n')
+        print('After typing your choice please press enter button\n')
         print('Please enter l to login using your username')
         print('or c to create an account')
 
-        answer = input('Enter your choice here:\n')
+        answer = input('Enter your choice here:')
         answer = answer.lower()
 
         if answer == 'l':
@@ -34,13 +36,34 @@ def welcome_screen():
 
 
 def login():
-    print('You have logged in')
+    """
+    login function first takes an username from the user and then
+    checks that against the usernames stored in the google worksheet.
+    """
+    while True:
+        answer = input('Enter your username and press enter:')
+        answer = answer.lower()
+        usernames = gs.get_usernames().keys()
+        if answer in usernames:
+            print('username found')
+            return False
+        else:
+            print(f'\n{answer} is not a valid username.')
+            print('would you like to create an account?')
+            print('Enter y for Yes and n for No')
+            new_answer = input('Enter choice here:')
+            if new_answer == 'y':
+                create_account()
+                return False
+            else:
+                print('Taking you back to the welcome screen....\n')
+                welcome_screen()
+                return False
 
 
 def create_account():
     print('Please create a new account')
 
 
-print('Welcome to the Ironworks Personal Training Booking System\n')
-print('\nAfter typing your choice please press enter button')
+
 welcome_screen()
