a7a56f96aba0146f92a7858e4b399068378e0f7c
Charlie Watson <charliewatson87@gmail.com>
2021-09-14 16:42:16 +0000

    feat: add validation of the user input for welcome_screen function

--- run.py
+++ run.py
@@ -7,34 +7,39 @@ All data required to be stored is done so on a google spreadsheet.
 
 import google_sheet
 
+
 def welcome_screen():
     """
-    Welcome message to the user along with an option to login or create an account
+    Welcome message to the user along with an option to login or create an
+    account
     """
     while True:
-        print('Please enter l to login using your username or c to create an account')
+        print('Please enter l to login using your username')
+        print('or c to create an account')
 
         answer = input('Enter your choice here:\n')
-
-        # validate_login(answer)
-
-        # if validate_login(answer):
+        answer = answer.lower()
 
         if answer == 'l':
+            print('Valid choice. Loading login.....')
             login()
-            break
-        else:
+            return False
+        
+        if answer == 'c':
+            print('Valid choice. Loading create account.....')
             create_account()
-            break
+            return False
+        
+        print(f'{answer} is an invalid entry, please use l or c')
 
-# def validate_login(data):
-#     try:
 
 def login():
     print('You have logged in')
 
+
 def create_account():
     print('Please create a new account')
+
 
 print('Welcome to the Ironworks Personal Training Booking System\n')
 welcome_screen()
