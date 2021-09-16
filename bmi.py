14ca670590160c89fba668d5820983afa5e2e18f
Charlie Watson <charliewatson87@gmail.com>
2021-09-16 00:21:29 +0000

    feat: create bmi.py for bmi calculation

---
+++ bmi.py
@@ -0,0 +1,25 @@
+"""
+bmi.py is used to hold the class for creating an instance of Person.
+"""
+
+
+class Person:
+    """
+    Creates an instance of Person
+
+    @param weight(float): one of the values needed to calculate bmi
+    @param height(float): one of the values needed to calculate bmi
+    """
+
+    def __init__(self, weight, height):
+        self.weight = weight
+        self.height = height
+
+    def calc_bmi(self):
+
+        # Calculates bmi from params
+        bmi = self.weight/((self.height/100)*(self.height/100))
+
+        # Rounds calculation to 2 decimal points
+        rounded_bmi = round(bmi, 2)
+        return rounded_bmi
